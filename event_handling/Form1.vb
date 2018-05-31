a3a51a37bc3b50812b118fdb2b619bf49a007db4
Jay Asbury <123@123.com>
2018-05-31 07:12:08 -0400

    Initial work

---
+++ event_handling/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim frm2 As New Form2
+        frm2.ShowDialog()
+    End Sub
+
+    ' Declare an event.
+    Public Event Ev_Event()
+
+    Sub CauseSomeEvent()
+        ' Raise an event.
+        RaiseEvent Ev_Event()
+        Label1.Text = "clicked on form 2"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ event_handling/Form2.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form2
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Sub TestEvents()
+        Dim Obj As New Form1
+        ' Associate an event handler with an event
+        AddHandler Obj.Ev_Event, AddressOf EventHandler
+        ' Call the method to raise the event
+        Obj.CauseSomeEvent()
+    End Sub
+
+    Sub EventHandler()
+        ' Handle the event.
+        MsgBox("success event")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call EventHandler()
+    End Sub
+End Class
